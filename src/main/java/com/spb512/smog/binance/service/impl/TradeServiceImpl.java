ba79f24d8d09191bd6b454f6fd3f66298291338d
spb512 <spb512@163.com>
2022-10-03 14:01:36 +0800

    1、添加初始化持仓标记逻辑。2、优化日志输出。3、格式化代码。

--- src/main/java/com/spb512/smog/binance/service/impl/TradeServiceImpl.java
+++ src/main/java/com/spb512/smog/binance/service/impl/TradeServiceImpl.java
@@ -1,6 +1,5 @@
 package com.spb512.smog.binance.service.impl;
 
-import com.alibaba.fastjson2.JSONObject;
 import com.binance.client.SyncRequestClient;
 import com.binance.client.exception.BinanceApiException;
 import com.binance.client.model.ResponseResult;
@@ -129,19 +128,20 @@ public class TradeServiceImpl implements TradeService {
     /**
      * 强制止损线
      */
-    private double stopLossLine = -0.10;
+    private double stopLossLine = -0.08;
     /**
      * 持仓模式："true": 双向持仓模式；"false": 单向持仓模式
      */
     String dual = "false";
+
     @PostConstruct
-    public void init(){
+    public void init() {
         publicClient = getClient.getPublicClient();
         privateClient = getClient.getPrivateClient();
         //初始化持仓模式
         try {
             ResponseResult result = privateClient.changePositionSide(dual);
-            if (result.getCode() == 200){
+            if (result.getCode() == 200) {
                 logger.info("初始化持仓模式为{}成功", dual);
             }
         } catch (BinanceApiException e) {
@@ -153,7 +153,7 @@ public class TradeServiceImpl implements TradeService {
         //初始化保证金模式
         try {
             ResponseResult marginTypeResult = privateClient.changeMarginType(symbolNam, marginType);
-            if (marginTypeResult.getCode() == 200){
+            if (marginTypeResult.getCode() == 200) {
                 logger.info("初始化保证金模式为{}成功", marginType);
             }
         } catch (BinanceApiException e) {
@@ -162,33 +162,36 @@ public class TradeServiceImpl implements TradeService {
         //初始化市价交易单次最大买和卖的数量
         ExchangeInformation exchangeInformation = publicClient.getExchangeInformation();
         List<ExchangeInfoEntry> symbols = exchangeInformation.getSymbols();
-        for(ExchangeInfoEntry exchangeInfoEntry : symbols){
-            if(symbolNam.equals(exchangeInfoEntry.getSymbol())){
+        for (ExchangeInfoEntry exchangeInfoEntry : symbols) {
+            if (symbolNam.equals(exchangeInfoEntry.getSymbol())) {
                 List<List<Map<String, String>>> filters = exchangeInfoEntry.getFilters();
                 List<Map<String, String>> mapList = filters.get(2);
                 maxQty = mapList.get(2).get("maxQty");
-                logger.info("最大数量maxQty：" + maxQty);
+                logger.info("市价最大开仓数量:" + maxQty);
             }
+        }
+        //初始化持仓标记
+        List<PositionRisk> positionRiskList = privateClient.getPositionRisk(symbolNam);
+        if (positionRiskList.get(0).getPositionAmt().doubleValue() > 0) {
+            isPosition = true;
+            logger.info("当前有持仓，初始化持仓标记isPosition为{}成功", isPosition);
         }
     }
 
     @Override
     public synchronized void openPosition() {
         // 当前是否有持仓
-        if(isPosition){
+        if (isPosition) {
             return;
         }
         //查询k线数据
         List<Candlestick> candlestickList = publicClient.getMarkPriceCandlesticks(symbolNam, interval, null, null, limit);
         IndicatorDto indicatorDto = getIndicators(candlestickList);
         double rsi12 = indicatorDto.getRsi12();
-//        logger.info("currentPrice:{}", candlestickList.get(candlestickList.size() - 1).getClose());
-//        logger.info("rsi12:{},highestHighRsi:{},lowestLowRsi{}", rsi12,highestHighRsi,lowestLowRsi);
         if ((rsi12 > activateHighRsi12) && (rsi12 > highestHighRsi)) {
             highestHighRsi = rsi12;
             logger.info("highestHighRsi更新，当前为:{}", highestHighRsi);
         }
-//        logger.info("(highestHighRsi > activateHighRsi12):{} && (highestHighRsi - rsi12 > pullbackRsi){},总的：{}",highestHighRsi > activateHighRsi12,(highestHighRsi - rsi12 > pullbackRsi),(highestHighRsi > activateHighRsi12) && (highestHighRsi - rsi12 > pullbackRsi));
         if ((highestHighRsi > activateHighRsi12) && (highestHighRsi - rsi12 > pullbackRsi)) {
             doSell = true;
         }
@@ -204,50 +207,54 @@ public class TradeServiceImpl implements TradeService {
 //        if (false) {
             //再次确认是否有持仓
             List<PositionRisk> positionRiskList = privateClient.getPositionRisk(symbolNam);
-            if(positionRiskList.get(0).getPositionAmt().doubleValue() > 0){
+            if (positionRiskList.get(0).getPositionAmt().doubleValue() > 0) {
                 return;
             }
-            //查询余额
-            List<AccountBalance> balanceList = privateClient.getBalance();
-            for (AccountBalance accountBalance : balanceList) {
-                if("USDT".equals(accountBalance.getAsset())){
-                    availableBalance = accountBalance.getAvailableBalance();
-                    if (availableBalance.compareTo(minStartup) < 0){
-                        doBuy = false;
-                        doSell = false;
-                        logger.info("账号余额:{},余额过低小于{}", availableBalance, minStartup);
-                        return;
-                    }
-                }
+            //查询更新余额变量
+            getBalance();
+            if (availableBalance.compareTo(minStartup) < 0) {
+                doBuy = false;
+                doSell = false;
+                logger.info("账号余额:{},余额过低小于{}", availableBalance, minStartup);
             }
             //计算开仓数量
-            BigDecimal currentPrice = candlestickList.get( candlestickList.size() - 1).getClose();
-            logger.info("当前余额:{}",availableBalance);
-            logger.info("当前价格:{}",currentPrice);
-            BigDecimal quantity = availableBalance.divide(currentPrice, 3, RoundingMode.HALF_UP).multiply(BigDecimal.valueOf(leverage)).multiply(BigDecimal.valueOf(0.8));
-            logger.info("下单数量{}", quantity);
+            BigDecimal currentPrice = candlestickList.get(candlestickList.size() - 1).getClose();
+            BigDecimal quantity = availableBalance.divide(currentPrice, 3, RoundingMode.HALF_UP).multiply(BigDecimal.valueOf(leverage)).multiply(BigDecimal.valueOf(0.7));
+            logger.info("当前价格:{};当前余额:{};下单数量{}", currentPrice, availableBalance, quantity);
             //下单
             OrderSide sid = OrderSide.BUY;
             String direction = "做多";
-            if (doSell){
-                sid =OrderSide.SELL;
+            if (doSell) {
+                sid = OrderSide.SELL;
                 direction = "做空";
             }
             Order order = privateClient.postOrder(symbolNam, sid, null, OrderType.MARKET, null, String.valueOf(quantity), null, null, null, null, null, null, null, null, null, NewOrderRespType.RESULT);
-            if (order.getOrderId() != null){
+            if (order.getOrderId() != null) {
                 isPosition = true;
                 lowestLowRsi = 100;
                 highestHighRsi = 0;
                 doBuy = false;
                 doSell = false;
             }
+            //查询更新余额变量
+            getBalance();
             logger.info("开{}仓,订单号ordId:{}=======>当前余额:{}", direction, order.getOrderId(), availableBalance);
+        }
+    }
+
+    private void getBalance() {
+        List<AccountBalance> balanceList = privateClient.getBalance();
+        for (AccountBalance accountBalance : balanceList) {
+            if ("USDT".equals(accountBalance.getAsset())) {
+                availableBalance = accountBalance.getAvailableBalance();
+                return;
+            }
         }
     }
 
     @NotNull
     private IndicatorDto getIndicators(List<Candlestick> candlestickList) {
-        for (int i = 0; i < candlestickList.size(); i++){
+        for (int i = 0; i < candlestickList.size(); i++) {
             dClose[i] = candlestickList.get(i).getClose().doubleValue();
         }
         FinStratEntity rsi12FinEntity = finModel.calRsi(dClose, 12);
@@ -258,33 +265,30 @@ public class TradeServiceImpl implements TradeService {
     }
 
     @Override
-    public synchronized void  closePosition() {
+    public synchronized void closePosition() {
         // 当前是否有持仓
         if (!isPosition) {
             return;
         }
         List<PositionRisk> positionRiskList = privateClient.getPositionRisk(symbolNam);
-        if(positionRiskList.get(0).getPositionAmt().doubleValue() == 0){
+        if (positionRiskList.get(0).getPositionAmt().doubleValue() == 0) {
             return;
         }
-        //查询k线数据
-        List<Candlestick> candlestickList = publicClient.getMarkPriceCandlesticks(symbolNam, interval, null, null, limit);
-        IndicatorDto indicatorDto = getIndicators(candlestickList);
-        double rsi12 = indicatorDto.getRsi12();
         PositionRisk positionRisk = positionRiskList.get(0);
         BigDecimal unrealizedProfit = positionRisk.getUnrealizedProfit();
         BigDecimal isolatedMargin = new BigDecimal(positionRisk.getIsolatedMargin());
         BigDecimal isolated = isolatedMargin.subtract(unrealizedProfit);
         BigDecimal uplRatio = unrealizedProfit.divide(isolated, 2, RoundingMode.HALF_UP);
-//        logger.info("UnrealizedProfit:{};uplRatio:{}",positionRisk.getUnrealizedProfit(), uplRatio);
         if ((uplRatio.compareTo(BigDecimal.valueOf(activateRatio)) > -1) && (uplRatio.compareTo(highestUplRatio) > 0)) {
             highestUplRatio = uplRatio;
-            logger.info("highestUplRatio更新，当前为:{};rsi12指数为:{}", highestUplRatio, rsi12);
+            logger.info("highestUplRatio更新，当前为:{}", highestUplRatio);
         }
         if ((highestUplRatio.compareTo(BigDecimal.valueOf(activateRatio)) > -1) && (uplRatio.compareTo(highestUplRatio.subtract(BigDecimal.valueOf(pullbackRatio))) < 1)) {
             Order order = sell(positionRisk);
-            logger.info("平仓成功,订单号ordId:{}=======>当前余额:{}",order.getOrderId(), availableBalance);
-            logger.info("平仓收益率为:{};rsi12指数为:{}", uplRatio, rsi12);
+            //查询更新余额变量
+            getBalance();
+            logger.info("平仓成功,订单号ordId:{};收益率为:{};当前余额:{}", order.getOrderId(), uplRatio, availableBalance);
+            logger.info("<=====================分隔符=======================>");
         }
     }
 
@@ -295,7 +299,7 @@ public class TradeServiceImpl implements TradeService {
             return;
         }
         List<PositionRisk> positionRiskList = privateClient.getPositionRisk(symbolNam);
-        if(positionRiskList.get(0).getPositionAmt().doubleValue() == 0){
+        if (positionRiskList.get(0).getPositionAmt().doubleValue() == 0) {
             return;
         }
         PositionRisk positionRisk = positionRiskList.get(0);
@@ -306,22 +310,22 @@ public class TradeServiceImpl implements TradeService {
         if (uplRatio.compareTo(BigDecimal.valueOf(stopLossLine)) < 0) {
             logger.info("达到强制止损线{}%", stopLossLine * 100);
             Order order = sell(positionRisk);
-            logger.info("当前收益率{}", uplRatio);
-            logger.info("平仓成功,订单号ordId:{}=======>当前余额:{}",order.getOrderId(), availableBalance);
+            logger.info("止损平仓成功,订单号ordId:{};收益率为:{};当前余额:{}", order.getOrderId(), uplRatio, availableBalance);
+            logger.info("<=====================分隔符=======================>");
         }
     }
 
     private Order sell(PositionRisk positionRisk) {
         BigDecimal positionAmt = positionRisk.getPositionAmt();
         OrderSide side;
-        if (positionAmt.doubleValue() > 0){
+        if (positionAmt.doubleValue() > 0) {
             side = OrderSide.SELL;
         } else {
             side = OrderSide.BUY;
         }
         Order order = privateClient.postOrder(symbolNam, side, null, OrderType.MARKET, null, positionAmt.abs().toString(), null, null, null, null, null, null, null, null, null, NewOrderRespType.RESULT);
         highestUplRatio = BigDecimal.ZERO;
-        if (order.getOrderId() != null){
+        if (order.getOrderId() != null) {
             isPosition = false;
         }
         return order;
